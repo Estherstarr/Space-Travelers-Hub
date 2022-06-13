d9bc019cf1d5136453f874b2882394678a4d334a
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-13 18:10:21 +0200

    create commponents folder

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../App.css';
+
+function Navbar() {
+  return (
+    <header>
+      <img class="logo" src='https://cdn-icons-png.flaticon.com/512/3212/3212567.png' alt="logo" />
+      <h2>Space Traveler's Hub</h2>
+      <nav>
+        <Link to="/rockets" className="menu-item1">Rockets</Link>
+        <Link to="/missions" className="menu-item2">Missions</Link>
+        <Link to="/" className="menu-item3">My profile</Link>
+      </nav>
+    </header>
+  )
+}
+
+export default Navbar
