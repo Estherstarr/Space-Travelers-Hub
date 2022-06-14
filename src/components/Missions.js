e0ccc61c3c1583fbc84db8937d9ebfb70f757990
GedeonTS <gedeontshobohwa1@gmail.com>
2022-06-14 09:57:07 +0200

    create table layout

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+
+const missionsAPI = 'https://api.spacexdata.com/v3/missions';
+fetch(missionsAPI).then((res) => res.json()).then((r) => console.log(r));
 
 function Missions() {
+  const data = [{
+    name: 'Ayaan',
+    age: 26
+  }, {
+    name: 'Ahana',
+    age: 22
+  }, {
+    name: 'Peter',
+    age: 40
+  }, {
+    name: 'Virat',
+    age: 30
+  }, {
+    name: 'Rohit',
+    age: 32
+  }, {
+    name: 'Dhoni',
+    age: 37
+  }];
+  const columns = [{
+    Header: 'Name',
+    accessor: 'name'
+  }, {
+    Header: 'Age',
+    accessor: 'age'
+  }]
+
   return (
-    <div>Missions</div>
+  
+      <ReactTable
+        data={data}
+        columns={columns}
+        defaultPageSize={2}
+        pageSizeOptions={[2, 4, 6]}
+      />
+    
   );
 }
 
