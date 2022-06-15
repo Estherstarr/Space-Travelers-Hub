0f9edc38a976a73b1e5ab0deeea220186f0de074
onomeh <onwuanibeonome@gmail.com>
2022-06-15 18:54:44 +0000

    Switch badges for rockets(reserved and canceled)

--- src/components/rocket.js
+++ src/components/rocket.js
@@ -1,18 +1,33 @@
 import './rocket.css';
 import PropTypes from 'prop-types';
 
-function Rocket({ name, description, flickrImages }) {
+function Rocket({
+  name, description, flickrImages, reservedRocket, reserved,
+}) {
   return (
     <div className="rocketContainer">
       <img src={flickrImages} alt="rocket" className="imgContainer" />
       <div className="displayInfo">
         <h1 className="headerText">{name}</h1>
-        <p className="descriptionTxt">
-          {description}
-        </p>
-        <button type="button" className="buttonStyle">
-          Reserve Rocket
-        </button>
+        <p className="descriptionTxt">{description}</p>
+        {!reserved && (
+          <button
+            type="button"
+            className="buttonStyle"
+            onClick={reservedRocket}
+          >
+            Reserve Rocket
+          </button>
+        )}
+        {reserved && (
+          <button
+            type="button"
+            className="buttonStyle"
+            onClick={reservedRocket}
+          >
+            Cancel Reservation
+          </button>
+        )}
       </div>
     </div>
   );
@@ -22,11 +37,14 @@ Rocket.propTypes = {
   name: PropTypes.string,
   description: PropTypes.string,
   flickrImages: PropTypes.string,
+  reserved: PropTypes.bool,
+  reservedRocket: PropTypes.func.isRequired,
 };
 
 Rocket.defaultProps = {
   name: '',
   description: '',
   flickrImages: '',
+  reserved: '',
 };
 export default Rocket;
