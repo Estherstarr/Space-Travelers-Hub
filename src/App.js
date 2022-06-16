8897513bce244c2ddbc02ac383beeabafcce1cfd
onomeh <onwuanibeonome@gmail.com>
2022-06-16 17:20:41 +0000

    Style css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/Profile';
+import { fetchRocket } from './redux/rockets/rockets';
 
 function App() {
+  const dispatch = useDispatch;
+  useEffect(() => {
+    dispatch(fetchRocket());
+  }, []);
   return (
     <section className="container">
       <Navbar />
