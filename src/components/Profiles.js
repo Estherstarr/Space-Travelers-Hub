7c9f2c87fb2eeef963c6520515efcaa8101c0ae2
onomeh <onwuanibeonome@gmail.com>
2022-06-17 18:01:38 +0000

    Fix linter error

--- src/components/Profiles.js
+++ src/components/Profiles.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Profile from './profile';
+import Profile from './Profile';
 
 function Profiles() {
   const rockets = useSelector((state) => state.rocketList);
